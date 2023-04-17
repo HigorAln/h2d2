04a9dc8c76c1856fea52e7c76c7edd5f68886301
Higor Allan <higor.allan21@gmail.com>
2023-04-17 10:53:36 -0300

    adding tsx

--- src/modules/ia/useCases/chatQuestion.ts
+++ src/modules/ia/useCases/chatQuestion.ts
@@ -1,15 +1,25 @@
-import { ChatInputCommandInteraction, CacheType } from "discord.js";
-import { chatgpt } from "../../../lib/ChatGPT";
-import { EmbedBuilder } from "discord.js";
+import { EmbedBuilder, ChatInputCommandInteraction, CacheType } from "discord.js";
 
 export async function chatQuestion(question: string, interaction: ChatInputCommandInteraction<CacheType>){
-  const response = await chatgpt.sendMessage(question)
+  try {
+    const { ChatGPTAPI } = await import("chatgpt");
+    console.log({ secret: process.env.SECRET_KEY! })
 
-  const embed = new EmbedBuilder()
-  .setColor(0x0099FF)
-  .setTitle(`Pergunta: ${question}`)
-  .setDescription(response.text)
-  .setTimestamp()
+    const api = new ChatGPTAPI({
+      apiKey: process.env.SECRET_KEY!
+    });
 
-  interaction.editReply({ embeds: [embed] })
+
+    const response = await api.sendMessage(question)
+
+    const embed = new EmbedBuilder()
+      .setColor(0x0099FF)
+      .setTitle(`Pergunta: ${question}`)
+      .setDescription(response.text)
+      .setTimestamp()
+  
+    interaction.editReply({ embeds: [embed] })
+  } catch(err) {
+    interaction.editReply(`Ocorreu um erro ao tentar executar o comando, Error: ${err}`)
+  }
 }
